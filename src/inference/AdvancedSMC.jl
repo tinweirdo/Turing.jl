fd1769f5de74ef5b346da4c9f4e11d11cb8d83a8
mohamed82008 <mohamed82008@gmail.com>
2020-01-15 19:34:41 +1100

    forgotten fix

--- src/inference/AdvancedSMC.jl
+++ src/inference/AdvancedSMC.jl
@@ -88,7 +88,7 @@ function sample_init!(
 
     # reset the VarInfo
     vi = spl.state.vi
-    reset_num_produce!(vi.num_produce)
+    reset_num_produce!(vi)
     set_retained_vns_del_by_spl!(vi, spl)
     resetlogp!(vi)
     empty!(vi)
